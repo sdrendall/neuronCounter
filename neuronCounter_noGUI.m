ee17815d85609b228afde29db75a5bd4349998f2
Sam Rendall <sdrendall@gmail.com>
2014-05-05 18:33:14 -0400

    restructured.  analyzeImagesInBuffer is no longer a nested function

--- neuronCounter_noGUI.m
+++ neuronCounter_noGUI.m
@@ -1,7 +1,7 @@
 function neuronCounter_noGUI(startPath)
 %% Script to analyze a set of FISH images without using the GUI
 % Ideally this can be run on Orchestra
-
+    
     if ~exist('startPath', 'var')
         startPath = uigetdir()
     end
@@ -12,6 +12,7 @@ function neuronCounter_noGUI(startPath)
     bufferInds = randperm(length(images));
 
     % Load Images
+    global imageBuffer
     imageBuffer = [];
     parfor i = 1:length(bufferInds)
         imageBuffer = [imageBuffer, bufferedImage(images(bufferInds(i)))];
@@ -25,29 +26,28 @@ function neuronCounter_noGUI(startPath)
     save('~/analyzedImages', 'imageBuffer')
 
 
-    function analyzeImagesInBuffer
-    
-        % Create some containers, to allow for parallelization
-        labIms = cell(size(imageBuffer));
-        neurons = labIms;
-        totalNeuronCounts = zeros(size(imageBuffer));
-    
-        % Analyze buffered images
-        for i = 1:length(imageBuffer)
-            [labIms{i}, neurons{i}] = findNeurons(imageBuffer(i).im);
-            totalNeuronCounts(i) = estimateTotalNeuronsFromDapi(imageBuffer(i).im);
-        end
-    
-        for i = 1:length(imageBuffer)
-            % Store values in data objects
-            imageBuffer(i).labIm = labIms{i};
-            imageBuffer(i).dataObj.neurons = neurons{i};
-            imageBuffer(i).dataObj.totalNeuronCount = totalNeuronCounts(i);
-    
-            % Spit out some values
-            disp(['totalNeuronCount: ', num2str(imageBuffer(i).dataObj.totalNeuronCount)])
-            disp(['% expressing neurons: ', num2str(imageBuffer(i).dataObj.percentNeuronsExpressing())])
-            disp('------------------------------------')
-        end
+function analyzeImagesInBuffer
+    global imageBuffer
+    % Create some containers, to allow for parallelization
+    labIms = cell(size(imageBuffer));
+    neurons = labIms;
+    totalNeuronCounts = zeros(size(imageBuffer));
+
+    % Analyze buffered images
+    for i = 1:length(imageBuffer)
+        [labIms{i}, neurons{i}] = findNeurons(imageBuffer(i).im);
+        totalNeuronCounts(i) = estimateTotalNeuronsFromDapi(imageBuffer(i).im);
     end
-end
+
+    for i = 1:length(imageBuffer)
+        % Store values in data objects
+        imageBuffer(i).labIm = labIms{i};
+        imageBuffer(i).dataObj.neurons = neurons{i};
+        imageBuffer(i).dataObj.totalNeuronCount = totalNeuronCounts(i);
+
+        % Spit out some values
+        disp(['totalNeuronCount: ', num2str(imageBuffer(i).dataObj.totalNeuronCount)])
+        disp(['% expressing neurons: ', num2str(imageBuffer(i).dataObj.percentNeuronsExpressing())])
+        disp('------------------------------------')
+    end
+    
